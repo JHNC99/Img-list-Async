6e897b1b5fc162596845c6751937b61b0afec7b4
JHNC99 <jonathanhnc75@gmail.com>
2021-05-06 14:58:34 -0500

    Modificacion correcta

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 // import { PropTypes } from 'react'
 import React from "react";
-import axios from 'axios';
+import axios from "axios";
 import ShearchInput from "./components/ShearchInput";
 class App extends React.Component {
+  state={images:[]}
   //Recibimoslos datos del componente SearchInput
-  onSearchSubmit=(entry)=>{
-    const response =  axios.get(`https://pixabay.com/api/?key=12973143-e1ea73fe98fc5af5156bb3056&q=${entry}&image_type=photo`)
-    console.log(response)
+    onSearchSubmit=async(entry)=> {
+    const response = await axios.get(`https://pixabay.com/api/?key=21500875-2f0e28f307b4b00a50bbbb07a&q=${entry}=photo
+    `);
+    this.setState({images:response.data.hits})
   }
   render() {
     return (
       <div className="ui container" style={{ marginTop: "35px" }}>
-        <ShearchInput onSearchSubmit={this.onSearchSubmit}/>
+        <ShearchInput onSearchSubmit={this.onSearchSubmit} />
+        We have {this.state.images.length} images
       </div>
     );
   }
